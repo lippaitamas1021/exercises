f493a15f9dead1fd2450bd712c24e0f2efd13e50
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-08-30 11:29:10 +0200

    Therapy - participant command

---
+++ therapies/src/main/java/therapies/therapy/AddParticipantCommand.java
@@ -0,0 +1,14 @@
+package therapies.therapy;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddParticipantCommand {
+
+    @NotNull
+    private int id;}
